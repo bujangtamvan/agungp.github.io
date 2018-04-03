3531e3448404fd26dd66cf3d1866e4c1c83c49f6
bujangtamvan <rendipasanova@gmail.com>
2018-04-03 16:21:28 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+
+<html>
+<embed src="http://www.youtube.com/v/jyauu-IDyWE?rel=0&autoplay=1&loop=1&playlist=qD8OnPC1fLI" type="application/x-shockwave-flash" width="0" height="0"</embed></object>
+<head>
+<title>Hacked by Mr.DreamX196</title> 
+<meta name="robots" content="index, follow"> 
+<link rel="SHORTCUT ICON" href="http://www.haurgeulis-security.com/favicon.ico"/> 
+<meta name="description" content="Hacked by Mr.DreamX196 Ft Mr.Vendetta_404 | Be Secure, Your Security Get Down!" />  
+<meta name="googlebot" content="index,follow" /> 
+<meta name="robots" content="all" /> 
+<meta name="robots schedule" content="auto" /> 
+<meta name="distribution" content="global" /> 
+</head>
+<body>
+<body bgcolor="#000000" text="white">
+<center><font size="6" color=cyan >Hacked by Mr.DreamX196 Ft. Mr.Vendetta_404</font></center>
+<center><br>
+<img src="https://pbs.twimg.com/media/CSvbBTbVAAE7tsP.jpg" height="339" width="599"><br>
+<p> <font face="courier new"><font size="4" color=red>
+This was not a joke or dream,<font size="4" color=yellow> this is fucking reality<br>
+<font size="4" color=white>Tidak ada seorangpun, hewan atau banci yang disakiti dalam hacking ini ;)<br><br>
+
+<small>
+<font color=gold><i>Made In Indonesia</i><br><br></font>
+<font color=" face="Tahoma" >sh00t to : </font><marquee direction=left behavior=alternate scrollamount="2" scrolldelay="20" width="50%"><font color="white" face="verdana">
+     Hmei7 | Unknown~X | Lazy Dot ID | rawinject_48 | Alpha~7 | 4Z15 | 3l-MUH4J1M1 | d2mysilent | R-404 HaX0r | Sayap Hitam | Gunz_Berry | ./Mr.Sk | Mr.xSaputra_AttackeRx | Gi~X | ~L~ | ./Denting_piano | Bujangtamvan | FadliDotID_007 | Mr.Vendetta_404 | Hawk_B404 | my self :D | Shani Indira Natio as a picture | Azu as a song | and you, yes you are that reading this..     
+</font></marquee><br>
+
+<br>
+WWW.MUSLIMCYBER.CO.ID
+<br>
+</small>
+</body></html>
