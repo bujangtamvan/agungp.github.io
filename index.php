3b96c82e381c2eb46057a5315219da136c01ae76
bujangtamvan <rendipasanova@gmail.com>
2018-04-03 18:20:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,16 @@
-
-<html>
-<embed src="http://www.youtube.com/v/jyauu-IDyWE?rel=0&autoplay=1&loop=1&playlist=qD8OnPC1fLI" type="application/x-shockwave-flash" width="0" height="0"</embed></object>
-<head>
-<title>Hacked by Mr.DreamX196</title> 
-<meta name="robots" content="index, follow"> 
-<link rel="SHORTCUT ICON" href="http://www.haurgeulis-security.com/favicon.ico"/> 
-<meta name="description" content="Hacked by Mr.DreamX196 Ft Mr.Vendetta_404 | Be Secure, Your Security Get Down!" />  
-<meta name="googlebot" content="index,follow" /> 
-<meta name="robots" content="all" /> 
-<meta name="robots schedule" content="auto" /> 
-<meta name="distribution" content="global" /> 
-</head>
-<body>
-<body bgcolor="#000000" text="white">
-<center><font size="6" color=cyan >Hacked by Mr.DreamX196 Ft. Mr.Vendetta_404</font></center>
-<center><br>
-<img src="https://pbs.twimg.com/media/CSvbBTbVAAE7tsP.jpg" height="339" width="599"><br>
-<p> <font face="courier new"><font size="4" color=red>
-This was not a joke or dream,<font size="4" color=yellow> this is fucking reality<br>
-<font size="4" color=white>Tidak ada seorangpun, hewan atau banci yang disakiti dalam hacking ini ;)<br><br>
-
-<small>
-<font color=gold><i>Made In Indonesia</i><br><br></font>
-<font color=" face="Tahoma" >sh00t to : </font><marquee direction=left behavior=alternate scrollamount="2" scrolldelay="20" width="50%"><font color="white" face="verdana">
-     Hmei7 | Unknown~X | Lazy Dot ID | rawinject_48 | Alpha~7 | 4Z15 | 3l-MUH4J1M1 | d2mysilent | R-404 HaX0r | Sayap Hitam | Gunz_Berry | ./Mr.Sk | Mr.xSaputra_AttackeRx | Gi~X | ~L~ | ./Denting_piano | Bujangtamvan | FadliDotID_007 | Mr.Vendetta_404 | Hawk_B404 | my self :D | Shani Indira Natio as a picture | Azu as a song | and you, yes you are that reading this..     
-</font></marquee><br>
-
-<br>
-WWW.MUSLIMCYBER.CO.ID
-<br>
-</small>
-</body></html>
+  <!DOCTYPE html>
+<html title="athour : agung p " oncontextmenu="return false;" onkeydown="return false;" onmousedown="return false;" onkeypress="return false;" onclick="return false;" ondragstart="return false;" onmousedown="return false();">
+<head><title>athour : agung p</title><meta name="google-site-verification" content="athour : agung p"><meta name="google-site-verification" content="athour : agung p by athour : agung p - WithOutShadow Team"><meta name="author" content="athour : agung p"><meta name="keywords" content="athour : agung p by athour : agung p,athour : agung p,alinko"><meta name="description" content="athour : agung p "><link rel="ICON" href="http://www.sadever.com/wp-content/uploads/160/sad-anime-boy-in-rain-750x422.jpg"><script type="text/javascript" src="https://sites.google.com/site/seocips/seocips-script/efek-salju-id.js" /></script><script type="text/javascript">snowStorm.snowColor = '#15f91b';snowStorm.flakesMaxActive = 96;snowStorm.useTwinkleEffect = true;</script></head>
+<!--+++++++++++++++++++++++++++++++
+    + c0ded by : shutdown57     +
+    + Jancok Ojo Nyolong Script +
+    + .athour : agung p@gmail.com
+    ++++++++++++++++++++++++++++++-->
+<body title="athour : agung p " onload="WithOutShadow_LoadHtml('athour : agung p');">
+<style type="text/css">@import 'https://fonts.googleapis.com/css?family=Kumar+One';
+*{font-family:'Kumar one',Cursive;cursor: pointer;}body{background:url('http://www.sadever.com/wp-content/uploads/160/sad-anime-boy-in-rain-750x422.jpg')no-repeat center fixed black;color: #15f91b;background-attachment: fixed;background-size: cover;}h1,p,h3{color: #15f91b;text-shadow: 0px 2px 10px #15f91b;}#kedua,#keempat{display: none;}h1{margin-top: 200px;font-size:2.8em;}h1:hover{background: #15f91b;color: #000;transition-duration: 3s;-o-transition-duration: 3s;-moz-transition-duration: 3s;-webkit-transition-duration: 3s;border-left:200px solid blue;}</style><script type="text/javascript">function WithOutShadow(kucing,anjing){document.getElementById(kucing).style.display='none';document.getElementById(anjing).style.display='block';}function WithOutShadow_LoadHtml(load){document.getElementById(load).innerHTML='<audio autoplay="1" loop="1"><source src="https://raw.githubusercontent.com/alintamvanz/alintamvanz.github.io/master/music/JKT48%20-%20Sakura%20No%20Hanabiratachi.mp3" type="audio/mp3"/></audio><h1 id="pertama" onmouseover=\'WithOutShadow("pertama","kedua");\'><font color=red>@YAELAHGUNG</font></h1><h1 id="kedua" onmouseleave=\'WithOutShadow("kedua","pertama");\'><font color=white>athour : agung p</font></h1><br><h3 id="ketiga" onmouseover=\'WithOutShadow(\"ketiga\",\"keempat\");\'>Akhirnya ada yang mengetahui kesendirianku selama ini,<br> dan pada akhirnya kamu mengetahuinya</h3><h3 id="keempat" onmouseleave=\'WithOutShadow("keempat","ketiga");\'>.athour : agung p@gmail.com</h3>';return false;}</script>
+<center>
+<div id="athour : agung p"></div>
+</center>
+</body>
+</html>
